97530b4088e72f854fd5aae593e5d3175e74af20
adamhylander <adamhylander8@gmail.com>
2021-10-14 21:13:36 +0200

    first commit

--- simplexskal.m
+++ simplexskal.m
@@ -10,7 +10,6 @@ B = A(:,bix);
 N = A(:,nix);
 cB = c(bix,:);
 cN = c(nix,:);
-xN = nix;
 opt=0;
 iter = 0;
 while opt==0
@@ -19,55 +18,56 @@ while opt==0
     % Beräkna reducerad kostnad och högerled
     % Beräkna mest negativ reducerad kostnad, rc_min,
     % och index för inkommande variabel, inkix
-    xB = inv(B) * b;
-    xN = 0;
+    xB = B\b;
     y = transpose(transpose(cB) * inv(B));
     z = transpose(b) * y;
     
     cHatN = cN - transpose(N) * y;
-    [rc_min, inkix] = min(cHatN);
+    %[rc_min, inkix] = min(cHatN);
     
-    %for r = 1:size(cN)
-    %    if r == 1
-    %        rc_min = cN(r);
-    %        inkix = r;
-    %    end
-    %    if rc_min > cN(r)
-    %        rc_min = cN(r);
-    %        inkix = r;
-    %    end
-    %end
+    for r = 1:size(cHatN)
+        if r == 1
+            rc_min = cHatN(r);
+            inkix = r;
+        end
+        if rc_min > cHatN(r)
+            rc_min = cHatN(r);
+            inkix = r;
+        end
+    end
+
+    
     if rc_min >= -1.0E-10
         opt=1;
         disp('Optimum');
     else
         % Beräkna inkommande kolumn, a
         % --------
-        a = N(:,inkix);
-        %a = B \ N(:,inkix);
+        %a = N(:,inkix);
+        %a = inv(B)*N(:,inkix);
+        a = B \ N(:,inkix);
         if max(a) <= 0 
             disp('Obegränsad lösning');
             return;
         else
             % Bestäm utgående variabel, utgix
             % -------
-            %for r = 1:size(a)
-                %if a(r) == 0
-                %    continue;
-                %end
-            %    if r == 1
-            %        quota = b(r)/a(r);
-            %        utgix = r;
-            %    end
-            %    if quota > b(r)/a(r)
-            %        quota = b(r)/a(r);
-            %        utgix = r;
-            %    end
-            %end
+            minValue = inf;
+            for i = 1:size(a)
+                if xB(i) ~= 0
+                    if xB(i)/a(i) > 0
+                        if xB(i)/a(i) < minValue
+                            minValue = xB(i)/a(i);
+                            utgix = i;
+                        end
+                    end
+                end
+            end
+                        
             
-            ratios = xB./a;
-            ratios(ratios < 0) = inf;
-            [utg, utgix] = min(ratios);
+            %ratios = xB./a;
+            %ratios(ratios < 0) = inf;
+            %[utg, utgix] = min(ratios);
 
 
             fprintf('Iter: %d, z: %f, rc_min: %f, ink: %d, utg: %d \n',iter,z,rc_min,nix(inkix),bix(utgix));
@@ -78,15 +78,21 @@ while opt==0
             bix(utgix) = nix(inkix);
             nix(inkix) = temporary;
 
-            xB = 0;
+            % Skapa initial partition
+            B = A(:,bix);
+            N = A(:,nix);
+            cB = c(bix,:);
+            cN = c(nix,:);
+            
+            %xB = 0;
 
-            temporary = B(:,utgix);
-            B(:,utgix) = N(:,inkix);
-            N(:,inkix) = temporary;
+            %temporary = B(:,utgix);
+            %B(:,utgix) = N(:,inkix);
+            %N(:,inkix) = temporary;
 
-            temporary = cB(utgix);
-            cB(utgix) = cN(inkix);
-            cN(inkix) = temporary;
+            %temporary = cB(utgix);
+            %cB(utgix) = cN(inkix);
+            %cN(inkix) = temporary;
 
         end
     end
